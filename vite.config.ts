3ffdd83ebee6322174a08a60d06e923f9a3f3f50
Lautaro Javier Fernandez Pricco <ljfp@ljfp.xyz>
2025-03-26 23:23:33 +0100

    Fixed bug importing Markdown module.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { plugin as markdown } from 'vite-plugin-markdown'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    markdown({ mode: 'react' })
+    markdown({ mode: ['react'] })
   ],
   resolve: {
     alias: {
