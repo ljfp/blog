3f3c50456ac76fa8db5045a45c6977e97f471432
Lautaro Javier Fernandez Pricco <ljfp@ljfp.xyz>
2025-03-27 03:08:51 +0100

    Fixed import mode for the Markdown plugin. Now it's being properly imported to be compatible with React.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { plugin as markdown } from 'vite-plugin-markdown'
+import { plugin as markdown, Mode } from 'vite-plugin-markdown'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    markdown({ mode: ['react'] })
+    markdown({ mode: [Mode.REACT] })
   ],
   resolve: {
     alias: {
